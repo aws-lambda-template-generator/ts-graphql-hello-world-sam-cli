7619eef12fa1a4d858b9bc8e12b5039a5facc835
mydatahack <thon6thon6@hotmail.com>
2022-03-12 12:32:09 +1100

    Update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,12 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
   },
   parser: '@typescript-eslint/parser',
-  extends: 'eslint:recommended',
+  extends: ['eslint-config-mdh-typescript-backend'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  rules: {
-    semi: ['error', 'always'],
-    quotes: [2, 'single'],
-    indent: ['error', 2]
-  }
 };
-  
